664ac38151042440b10ac8cedb8a2c45d1b67ad9
Hoksolinvan <chhunhoksolinvan.prof@gmail.com>
2024-12-26 22:57:53 -0800

    Simple Socket programming

---
+++ messenger/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const {createServer} = require('node:http');
+const PORT = process.env.PORT || 3012;
+const {join} = require('node:path');
+const {Server} = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/',(req,res)=>{
+    res.sendFile(join(__dirname,'index.html'));
+});
+
+
+io.on('connection',(socket) =>{
+
+
+
+    socket.on('messages',(arg) =>{
+        console.log(arg);
+
+        io.emit('messages',arg);
+    });
+
+    
+
+    
+   console.log('a user has connected');
+
+});
+
+
+
+
+server.listen(PORT,()=>{
+    console.log('server is ready!');
+});
